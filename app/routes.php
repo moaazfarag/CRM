6adc3309503d51ecfd3d06171b87c37fe422ae8f
ahmed_elnoor <ahmedelnoor22@gmail.com>
2015-06-29 13:47:56 +0200

    add d char

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-    Route::get('/', 'HomeController@index');
+    Route::get('/', 'HomeControlledr@index');
     Route::group(array('prefix'=>'admin'),function(){
 
         Route::get('/','dashboardController@index');
