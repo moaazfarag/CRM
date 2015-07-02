5d79baf294b712e62173811baada818527b60e5e
mohamedhafez <mohamed.hafezqo@gmail.com>
2015-07-02 17:05:06 +0200

    user model -> hasMany

--- app/models/User.php
+++ app/models/User.php
@@ -23,4 +23,48 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
 
+
+
+    public function branch()
+
+    {
+        return $this->hasMany('Branches','user_id');
+    }
+
+    public function cat()
+
+    {
+        return $this->hasMany('Cat','user_id');
+    }
+
+    public function seasons()
+
+    {
+        return $this->hasMany('Seasons','user_id');
+    }
+
+    public function marks()
+
+    {
+        return $this->hasMany('Marks','user_id');
+    }
+
+    public function models()
+
+    {
+        return $this->hasMany('Models','user_id');
+    }
+
+    public function items()
+
+    {
+        return $this->hasMany('Items','user_id');
+    }
+    public function accounts()
+
+    {
+        return $this->hasMany('Accounts','user_id');
+    }
+
+
 }
