c4261c95d59c155f166bb928fda3aaa5d326ce90
Ahmed Bahaa <ah.bahaa@outlook.com>
2015-07-26 16:10:06 +0200

    aside open list

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -13,6 +13,7 @@ class AccountController extends BaseController
             if($this->checkType($accountType)) {
             $data['rowsData'] = Accounts::where('acc_type','=',$accountType)->get();
             $data['accountType'] = $accountType;
+            $data['asideOpen']   = 'open' ;
             $data['navActive'] = "active";
             return View::make('dashboard.account_bank',$data);
             }else{
@@ -24,6 +25,7 @@ class AccountController extends BaseController
             if($this->checkType($accountType)) {
             $data['rowsData'] = Accounts::where('acc_type','=',$accountType)->get();
             $data['accountType'] = $accountType;
+            $data['asideOpen']   = 'open' ;
             $data['account'] = Accounts::where('acc_type','=',$accountType)->where('id','=',$id)->first();
             if($data['account']) {
                 return View::make('dashboard.account_bank', $data);
