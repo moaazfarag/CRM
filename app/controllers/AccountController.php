5e4961c04c085a9c213218aa05283ff273e859cd
ahmed_elnoor <ahmedelnoor22@gmail.com>
2016-01-18 11:58:07 +0200

    resolve daily treasury bug

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -1040,8 +1040,8 @@ class AccountController extends BaseController
                     }// end else
                 }
 
-                $data['last_debit'] = array_sum($credit);
-                $data['last_credit'] = array_sum($debit);
+                $data['last_debit'] = array_sum($debit);
+                $data['last_credit'] = array_sum($credit);
 
             }else{
 
@@ -1102,8 +1102,8 @@ class AccountController extends BaseController
                         }// end else
                     }
 
-                    $data["last_debit"][$branch->id]  = array_sum($credit);
-                    $data["last_credit"][$branch->id] = array_sum($debit);
+                    $data["last_debit"][$branch->id]  = array_sum($debit);
+                    $data["last_credit"][$branch->id] = array_sum($credit);
 
                 }else{
 
